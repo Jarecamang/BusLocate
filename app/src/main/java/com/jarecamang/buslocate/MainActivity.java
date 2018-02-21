2834e583c8bdf9078b3e1200a042088561cbd7da
jarecamang <jarecamang@unal.edu.co>
2018-02-20 22:34:12 -0500

    Already Working JSON parser with NO UI.

--- app/src/main/java/com/jarecamang/buslocate/MainActivity.java
+++ app/src/main/java/com/jarecamang/buslocate/MainActivity.java
@@ -1,13 +1,103 @@
 package com.jarecamang.buslocate;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
+import java.net.HttpURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = MainActivity.class.getSimpleName();
+    private TextView mBusList;
+    String test = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBusList = (TextView) findViewById(R.id.bus_data);
+        mBusList.append("Well it's kinda working\n\n\n");
+        mBusList.append("Yes it is\n\n\n");
+        mBusList.append("OMG\n\n\n");
+        mBusList.append("hahahah\n\n\n");
+        mBusList.append("hahahah\n\n\n");
+        new GetBusData().execute();
+    }
+
+    private class GetBusData extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this,"Json Data is downloading", Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "https://api.myjson.com/bins/10yg1t";
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray buses = jsonObj.getJSONArray("school_buses");
+
+                    // looping through All Buses
+                    for (int i = 0; i < buses.length(); i++) {
+                        JSONObject b = buses.getJSONObject(i);
+                        //mBusList.append(b.getString("id"));
+                        Log.v(TAG, b.getString("id"));
+                        //debe guardar en algún lado para luego mostrar en el postexecute
+                        test += b.getString("id");
+
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            mBusList.append(test);
+        }
     }
 }
